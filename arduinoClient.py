a5b71cb029fa504547826183ec2d1d20021b74e9
Alex <alex@decoded.co>
2015-06-24 16:49:04 +0100

    adding script README and MIT license

---
+++ arduinoClient.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+'''
+The MIT License (MIT)
+
+Copyright (c) <year> <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+import re
+import sys
+import glob
+import serial
+import urllib2
+import signal
+
+#DecodedCo
+#author: Alex Walker
+# june 2015
+
+#store the baud rate
+baudRate = 0
+#list serial ports
+def serial_ports():
+
+    if sys.platform.startswith('win'):
+        ports = ['COM' + str(i + 1) for i in range(256)]
+
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this is to exclude your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+    #store the results in a list
+    result = []
+    for port in ports:
+        try: #catch errors
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+def readSerialData(port):
+    ser = serial.Serial(port, 9600)
+    while True:
+        input = ser.readline()
+        print input
+        # print "Received: ", input +  " -- looking for urls..."
+        #find urls in returned data
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', input)
+        for u in urls:
+            #go to url
+            print "found and going to: ", u
+            print urllib2.urlopen(u).read()
+
+#this is so that a CTRL C doesnt output a horrible error...
+def exit_gracefully(signum, frame):
+    # restore the original signal handler as otherwise evil things will happen
+    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    signal.signal(signal.SIGINT, original_sigint)
+
+    try:
+        if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+            "Goodbye!"
+            sys.exit(1)
+
+    except KeyboardInterrupt:
+        print("Ok ok, quitting")
+        sys.exit(1)
+
+    # restore the exit gracefully handler here    
+    signal.signal(signal.SIGINT, exit_gracefully)
+
+if __name__ == '__main__':
+    global baudRate
+    if len(sys.argv) < 2:
+        print "Please put a baud rate"
+        sys.exit(1)
+    # store the original SIGINT handler 
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, exit_gracefully)
+    baudRate = sys.argv[1] 
+    print "Baud rate is set to: " + baudRate
+    ports = serial_ports()
+    for i in ports:
+        if "tty.usbmodem" in i:
+            readSerialData(i)
+            break
